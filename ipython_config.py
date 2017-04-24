c5e7c2d7c6863fe48d92408d216d46e2b7035353
Jessie <mxjessie@users.noreply.github.com>
2017-04-24 16:45:55 -0700

    add tmux config, powerline it & also powerline ipython

--- ipython_config.py
+++ ipython_config.py
@@ -1,6 +1,10 @@
+from powerline.bindings.ipython.since_5 import PowerlinePrompts
 c = get_config()
 
 c.InteractiveShell.autoindent = True
 c.InteractiveShell.editor = 'vim'
 c.InteractiveShellApp.extensions = ['autoreload']
 c.InteractiveShellApp.exec_lines = ['%autoreload 2']
+
+c.TerminalInteractiveShell.simple_prompt = False
+c.TerminalInteractiveShell.prompts_class = PowerlinePrompts
